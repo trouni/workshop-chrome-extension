9343533891c40ba122b0c6c5d0ef16469bc5f5e0
Trouni Tiet <trouni@gmail.com>
2020-11-18 17:18:09 +0900

    simplified workshop (removed API section)

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,13 +7,3 @@ function sendCheesifyMsg() {
 }
 
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
-
-
-// ======================================================= //
-
-// TODO: Write a function to send a message containing the current page's title & url to our background script
-function addItemToList() {
-}
-
-// TODO: Add an event listener to trigger the function above when clicking the 'Add to Read List' button
-
